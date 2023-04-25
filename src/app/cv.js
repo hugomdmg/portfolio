86818bd2446b454aa8de4ce6d61b212201898b1f
hugomdmg <hugomdmg@gmail.com>
2023-04-25 11:51:27 +0200

    pdf refactor

--- src/app/cv.js
+++ src/app/cv.js
@@ -3,7 +3,7 @@ export default function CV() {
 
     return (
         <div className="pdfContainer">
-            <object id="pdfHolder" data="./cv-hugo.pdf" type="application/pdf" width="1100" height="600">
+            <object id="pdfHolder" data={require("./cv-hugo.pdf")} type="application/pdf" width="1100" height="600">
             </object>
         </div>
     )
